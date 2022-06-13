1c6d43366a2c282abc3885efc4b10d299149809d
gabinavalon <gabinavalon@gmail.com>
2022-06-13 20:24:26 +0200

    Cambios conexion para server

--- app/model/ConexionBD.php
+++ app/model/ConexionBD.php
@@ -7,8 +7,19 @@
  */
 class ConexionBD {
     public static function conectar(): mysqli{
+
+        // Configuración Local 
+        /*
         $conn = new mysqli('localhost','root','','ceramica_tejares');
 
+        if($conn->connect_error){
+            die("Error al conectar con MySQL: " . $conn->error);
+        }
+        return $conn;*/
+
+        // Configuración Servidor
+
+        $conn = new mysqli('localhost','sql_ceramicateja', passwordBD,'sql_ceramicateja');
         if($conn->connect_error){
             die("Error al conectar con MySQL: " . $conn->error);
         }
